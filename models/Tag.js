752266b7b594cc0deffbb530fa574adc4af24ad6
Katelyn Bunker <kerbunker@gmail.com>
2022-01-15 17:58:53 -0500

    add product routes

--- models/Tag.js
+++ models/Tag.js
@@ -7,7 +7,7 @@ class Tag extends Model {}
 Tag.init(
   {
     id: {
-      type: DataTypes.Integer,
+      type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
       autoIncrement: true
